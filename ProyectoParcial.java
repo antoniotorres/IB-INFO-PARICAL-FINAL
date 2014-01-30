9fa06b1705aaeadf57a9affaa8c0b163b9417ef7
esesupermario <marioivanguti@gmail.com>
2014-01-29 22:15:42 -0600

    Update ProyectoParcial.java

--- ProyectoParcial.java
+++ ProyectoParcial.java
@@ -1,7 +1,7 @@
 /**
  * @(#)ProyectoParcial.java
  * 
- * @author (Jose Antonio Torres -A01193080 y Mario Gutierrez - A0119)
+ * @author (Jose Antonio Torres -A01193080 y Mario Gutierrez - A01039315)
  * @version 1.00 01/29/2014
  * 
  * @licencia GPLv2
@@ -42,13 +42,34 @@ public class ProyectoParcial
     }
     
   }
+  
+  public static void selectionSortUpDown (int[] list) {
+    int min;
+    for (int i = 0; i < list.length; i++) {
+      // Asumiendo el primer arreglo es el menor
+      min = i;
+      for (int j = i + 1; j < list.length; j++) {
+        if (list[j] < list[min]) {
+          min = j;
+          
+        }
+      }
+      if (min != i) {
+        final int temp = list[i];
+        list[i] = list[min];
+        list[min] = temp;
+      }
+      System.out.println(list[i]);// I print the in ascending orde
+    }
+    
+  }
   public static void main (String [] args) throws IOException
   {
     int num=10000;
     int[] list= new int[num];
     list=generateRandom(num);
     selectionSortDownUp(list);
-    //selectionSortUpDown(list);
+    selectionSortUpDown(list);
     stdOut.println("Dame el primer valor");
   }
 }
