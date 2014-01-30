8018848b87c9c3b5102c000ee782e019f5440dc2
Antonio <thetonio96@gmail.com>
2014-01-29 22:10:02 -0600

    Update ProyectoParcial.java
    Agregue selection sort de menor a mayor.

--- ProyectoParcial.java
+++ ProyectoParcial.java
@@ -18,13 +18,37 @@ public class ProyectoParcial
   
   public static int[] generateRandom(int num){
     int[] list= new int[num];
+    for(int i=0; i < list.length; i++)
+      list[i]=(int)(Math.random()*10000);
     return list;
+  }
+  public static void selectionSortDownUp (int[] list) {
+    int min;
+    for (int i = 0; i < list.length; i++) {
+      // Asumiendo el primer arreglo es el menor
+      min = i;
+      for (int j = i + 1; j < list.length; j++) {
+        if (list[j] < list[min]) {
+          min = j;
+          
+        }
+      }
+      if (min != i) {
+        final int temp = list[i];
+        list[i] = list[min];
+        list[min] = temp;
+      }
+      System.out.println(list[i]);// I print the in ascending orde
+    }
+    
   }
   public static void main (String [] args) throws IOException
   {
     int num=10000;
     int[] list= new int[num];
     list=generateRandom(num);
-    stdOut.println("Dame el primer valor"+list[10000]);
+    selectionSortDownUp(list);
+    //selectionSortUpDown(list);
+    stdOut.println("Dame el primer valor");
   }
 }
